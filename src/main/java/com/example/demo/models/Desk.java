9551106d8093ffdcb13bf5fd13350d625e1ba7dd
Bogdan <pupezab@yahoo.com>
2023-05-15 09:25:45 +0300

    Update Desk.java

--- src/main/java/com/example/demo/models/Desk.java
+++ src/main/java/com/example/demo/models/Desk.java
@@ -10,7 +10,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.websocket.OnClose;
 
 @Entity
 public class Desk extends DataModel{
